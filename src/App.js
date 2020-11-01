e985cc1264a7f2aae1126c1d297431da69148058
Melinda Ivanov <jns.melinda@gmail.com>
2020-10-31 20:08:37 -0700

    project structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+      return (
+        <div className="App">
+          <h1>Employee Directory</h1>
+        </div>
+      );
+    }
+  }
+
+export default App;
